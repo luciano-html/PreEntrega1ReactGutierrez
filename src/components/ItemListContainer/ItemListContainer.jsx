eb0b9a4fe20041298143a397dfd8cc021cddfd01
Luciano Gutierrez <luciano.c.gutierrez@gmail.com>
2023-07-21 16:12:15 -0300

    rama fix

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,14 +5,13 @@ import "./itemListContainerStyles.css"
 import ButtonComponent from "../NavBar/ButtonComponent/ButtonComponent";
 import ItemList from "./ItemList";
 
-
 function ItemListContainer(props) {
     // 1 OBTENER DE FORMA ASYNC LA LISTA DEL ECOMMERCE
     // 2 GUARDAMOS LA RESPUESTA EN UN ESTADO
     // 3 USAMOS KEY PARA EL MAP
     // 4
     const [products, setProducts] = useState([])
-    console.log("Renderizando item list container")
+
     async function requestData() {
         const respuesta = await getData()
         setProducts(respuesta)
@@ -24,16 +23,13 @@ function ItemListContainer(props) {
         },
         []
     )
-
-
-
     return (
         <div className="itemContainer">
             <h1 className="greeting" >{props.greeting}</h1>
             {products.map((item) => (
                 <Item key={item.id} {...item} />
             ))}
-            
+
         </div>
     )
 }
